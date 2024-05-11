bace865f895c561528d7f2937e3e6a89fb54ff2a
AngeloAyranji <angeloayranji@gmail.com>
2024-05-11 18:10:38 +0300

    fix: fix docker compose and scripts

--- scripts/run-dev-uniswap.sh
+++ scripts/run-dev-uniswap.sh
@@ -5,21 +5,19 @@ set -e
 source scripts/util/prepare-env.sh
 
 # Add dev prefix
-export PROJECT_NAME="$PROJECT_NAME-dev"
+export PROJECT_NAME="$PROJECT_NAME-uniswap-dev"
 export DATA_DIR="$DATA_DIR-dev"
 
 source scripts/util/compose-cleanup.sh
 
-echo "Building containers..."
+echo "Building and starting containers..."
 docker compose \
     -p $PROJECT_NAME \
     -f docker-compose.base.yml \
     -f docker-compose.yml \
-    --build \
-    -d
+    --profile uniswap up --build -d
 
-# attach the logs only to uniswap
+echo "Attaching logs to uniswap..."
 docker compose \
     -p $PROJECT_NAME \
-    logs \
-    -f uniswap
+    logs -f uniswap &
