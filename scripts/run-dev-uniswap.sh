1bac8c7ec259032678c9b16552fd9128162180b9
AngeloAyranji <angeloayranji@gmail.com>
2024-05-11 17:57:01 +0300

    feat: add scripts

---
+++ scripts/run-dev-uniswap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+source scripts/util/prepare-env.sh
+
+# Add dev prefix
+export PROJECT_NAME="$PROJECT_NAME-dev"
+export DATA_DIR="$DATA_DIR-dev"
+
+source scripts/util/compose-cleanup.sh
+
+echo "Building containers..."
+docker compose \
+    -p $PROJECT_NAME \
+    -f docker-compose.base.yml \
+    -f docker-compose.yml \
+    --force-recreate \
+    --build \
+    --remove-orphans \
+    -d
+
+# attach the logs only to uniswap
+docker compose \
+    -p $PROJECT_NAME \
+    logs \
+    -f uniswap
