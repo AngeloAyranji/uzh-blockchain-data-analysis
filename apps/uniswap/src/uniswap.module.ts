072f2506c55e09c8ba8bae644f29a76c5778c980
AngeloAyranji <angeloayranji@gmail.com>
2024-05-25 16:21:52 +0300

    feat(uniswap): add redis to docker + bullmq setup

--- apps/uniswap/src/uniswap.module.ts
+++ apps/uniswap/src/uniswap.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { CollectionDbModule } from '@uzh/collection-db';
 import { UniswapDbHandler } from './infra/db/uniswap-db.handler';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [CollectionDbModule],
   controllers: [],
+  imports: [
+    CollectionDbModule,
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'extract',
+    }),
+    BullModule.registerQueue({
+      name: 'transform',
+    }),
+    BullModule.registerQueue({
+      name: 'load',
+    }),
+  ],
   providers: [UniswapDbHandler],
 })
 export class UniswapModule {}
