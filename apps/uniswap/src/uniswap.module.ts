2e18b0b0d31664c726f69005cd3a6fd3dbc10011
AngeloAyranji <angeloayranji@gmail.com>
2024-05-25 12:41:55 +0300

    fix: small changes

--- apps/uniswap/src/uniswap.module.ts
+++ apps/uniswap/src/uniswap.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { CollectionDbModule } from "@uzh/collection-db";
 
 @Module({
-    imports: [],
+    imports: [CollectionDbModule],
     controllers: [],
     providers: [],
     exports: []
