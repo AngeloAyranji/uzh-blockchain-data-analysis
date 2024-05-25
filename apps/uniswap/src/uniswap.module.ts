2659d7cd339b6147fc5c019c8573436af0badaeb
AngeloAyranji <angeloayranji@gmail.com>
2024-05-25 12:44:44 +0300

    feat(uniswap): add db handler service

--- apps/uniswap/src/uniswap.module.ts
+++ apps/uniswap/src/uniswap.module.ts
@@ -1,10 +1,10 @@
-import { Module } from "@nestjs/common";
-import { CollectionDbModule } from "@uzh/collection-db";
+import { Module } from '@nestjs/common';
+import { CollectionDbModule } from '@uzh/collection-db';
+import { UniswapDbHandler } from './infra/db/uniswap-db.handler';
 
 @Module({
-    imports: [CollectionDbModule],
-    controllers: [],
-    providers: [],
-    exports: []
+  imports: [CollectionDbModule],
+  controllers: [],
+  providers: [UniswapDbHandler],
 })
 export class UniswapModule {}
