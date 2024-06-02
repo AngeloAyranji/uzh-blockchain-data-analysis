1a3a36f1f23df349e73b4b79a0a55228635e1980
AngeloAyranji <angeloayranji@gmail.com>
2024-06-02 18:01:29 +0300

    feat(uniswap): add json-db

--- apps/uniswap/src/core/applications/migration/extract/extract.processor.ts
+++ apps/uniswap/src/core/applications/migration/extract/extract.processor.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
+import JSONdb from 'simple-json-db';
 import { IExtractProcessor } from './iextract.processor';
 import {
   FACTORY_READ_SERVICE,
@@ -25,52 +26,70 @@ export class ExtractProcessor implements IExtractProcessor {
     @Inject(LOG_READ_SERVICE)
     private readonly logReadService: ILogReadService,
 
-    private readonly config: ConfigService,
+    private readonly config: ConfigService
   ) {
     this.startMigration();
   }
 
   async startMigration() {
-    Logger.log("Migration started")
+    Logger.log('Migration started');
     const chainId = this.retreiveChainId();
     const factories = await this.factoryReadService.findAllByChainId(chainId);
-    
+
     await this.extractPools(factories);
-    Logger.log("Migration finished")
+    Logger.log('Migration finished');
   }
-  
+
   async extractPools(factories: Factory[]): Promise<void> {
     for (const factory of factories) {
-      let lastTransactionHash = undefined;
-      let lastLogIndex = undefined;
       let moreLogs = true;
       const pageSize = 20;
 
       while (moreLogs) {
+        const cursor = this.getCursor('pool');
+
         const logs = await this.logReadService.findLogsByTopic0AndAddress(
           factory.address,
           factory.poolCreatedSignature,
           pageSize,
-          lastTransactionHash,
-          lastLogIndex
+          cursor?.lastTransactionHash,
+          cursor?.lastLogIndex
         );
-        console.log(logs.length)
+        console.log(logs.length);
         if (logs.length > 0) {
           // TODO: map logs to PairCreatedRequest
-          this.transformQueue.add(`POOL_CREATED_${factory.version}`, {
-            logs: logs,
-            factoryId: factory.id,
-          }, {
-            removeOnComplete: true,
-          });
+          this.transformQueue.add(
+            `POOL_CREATED_${factory.version}`,
+            {
+              logs: logs,
+              factoryId: factory.id,
+            },
+            {
+              removeOnComplete: true,
+            }
+          );
 
-          lastTransactionHash = logs[logs.length - 1].transactionHash;
-          lastLogIndex = logs[logs.length - 1].logIndex;
+          const lastTransactionHash = logs[logs.length - 1].transactionHash;
+          const lastLogIndex = logs[logs.length - 1].logIndex;
+          this.setCursor('pool', {
+            lastTransactionHash,
+            lastLogIndex,
+          });
         }
 
         moreLogs = logs.length === pageSize;
       }
     }
+  }
+
+  private getCursor(key: string): any {
+    const cursorDb = new JSONdb('./apps/uniswap/json-db.json');
+    return cursorDb.get(key);
+  }
+
+  private setCursor(key: string, value: any): void {
+    const cursorDb = new JSONdb('./apps/uniswap/json-db.json');
+    cursorDb.set(key, value);
   }
 
   private retreiveChainId(): number {
